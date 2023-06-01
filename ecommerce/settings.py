fbff14a494d122cd15490fbe50da910d5962e991
Usamaalzomor <osama.zomor2010@gmail.com>
2023-06-01 22:09:09 +0300

    feat: Migrate and connect project to MySQL Database

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -84,12 +84,20 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+
+DATABASES = {  
+    'default': {  
+        'ENGINE': 'django.db.backends.mysql',  
+        'NAME': 'ecommerce',  
+        'USER': 'root',  
+        'PASSWORD': 'Osa_05910120',  
+        'HOST': '127.0.0.1',  
+        'PORT': '3306',  
+        'OPTIONS': {  
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"  
+        }  
+    }  
+}  
 
 
 # Password validation
