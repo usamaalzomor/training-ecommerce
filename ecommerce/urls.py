d366c20da267f6cee556bea23a77115a63da81f7
Usamaalzomor <osama.zomor2010@gmail.com>
2023-06-02 23:36:35 +0300

    feat(store): Add API endpoints for product listing and single product details

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -1,8 +1,7 @@
-"""
-URL configuration for ecommerce project.
+"""storefront URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -16,9 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+import debug_toolbar
+
+admin.site.site_header = 'Storefront Admin'
+admin.site.index_title = 'Admin'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('playground/', include('playground.urls')),
-    path('__debug__/', include('debug_toolbar.urls')),
+    path('store/', include('store.urls')),
+    path('__debug__/', include(debug_toolbar.urls)),
 ]
