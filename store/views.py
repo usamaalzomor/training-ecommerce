120f520c8a9954605664db5e2377974525853995
Usamaalzomor <osama.zomor2010@gmail.com>
2023-06-10 23:58:12 +0300

    refactor(store): Simplify CollectionDetail view with DRF's RetrieveUpdateDestroyAPIView

--- store/views.py
+++ store/views.py
@@ -32,21 +32,16 @@ class CollectionList(ListCreateAPIView):
     serializer_class = CollectionSerializer
 
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def collection_detail(request, pk):
-    collection = get_object_or_404(
-        Collection.objects.annotate(
-            products_count=Count('product')), pk=pk)
-    if request.method == 'GET':
-        serializer = CollectionSerializer(collection)
-        return Response(serializer.data)
-    elif request.method == 'PUT':
-        serializer = CollectionSerializer(collection, data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data)
-    elif request.method == 'DELETE':
-        if collection.products.count() > 0:
+class CollectionDetail(RetrieveUpdateDestroyAPIView):
+    queryset = Collection.objects.annotate(products_count=Count('product'))
+    serializer_class = CollectionSerializer
+
+    def delete(self, request, pk):
+        collection = get_object_or_404(
+            Collection.objects.annotate(
+                products_count=Count('product')), pk=pk)
+        if collection.product_set.count() > 0:
             return Response({'error': 'Collection cannot be deleted because it includes one or more products.'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
-        collection.delete()
+            collection.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+    
