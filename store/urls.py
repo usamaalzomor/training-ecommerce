b82b7d33ed3c685bed28a49a5c269f0fe01c271b
Usamaalzomor <osama.zomor2010@gmail.com>
2023-06-11 19:37:50 +0300

    refactor(store): Simplify views with DRF's ModelViewSet

--- store/urls.py
+++ store/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views
+from rest_framework.routers import DefaultRouter
+from .views import ProductViewSet, CollectionViewSet
+
+router = DefaultRouter()
+router.register(r'products', ProductViewSet)
+router.register(r'collections', CollectionViewSet)
 
 # URLConf
-urlpatterns = [
-    path('products/', views.ProductList.as_view()),
-    path('products/<int:pk>/', views.ProductDetail.as_view()),
-    path('collections/', views.CollectionList.as_view()),
-    path('collections/<int:pk>/', views.CollectionDetail.as_view(), name='collection-detail'),
-]
+urlpatterns = router.urls
--- store/views.py
+++ store/views.py
@@ -4,18 +4,13 @@ from django.http import HttpResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.views import APIView 
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.viewsets import ModelViewSet
 from .models import Collection, Product
 from .serializers import ProductSerializer, CollectionSerializer
 
 
-class ProductList(ListCreateAPIView):
-    queryset = Product.objects.select_related('collection').all()
-    serializer_class = ProductSerializer
-
-
-class ProductDetail(RetrieveUpdateDestroyAPIView):
+class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
@@ -24,24 +19,16 @@ class ProductDetail(RetrieveUpdateDestroyAPIView):
         if product.orderitems.count() > 0:
             return Response({'error': 'Product cannot be deleted because it is associated with an order item.'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
         product.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)   
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
-class CollectionList(ListCreateAPIView):
+class CollectionViewSet(ModelViewSet):
     queryset = Collection.objects.annotate(products_count=Count('product')).all()
     serializer_class = CollectionSerializer
 
-
-class CollectionDetail(RetrieveUpdateDestroyAPIView):
-    queryset = Collection.objects.annotate(products_count=Count('product'))
-    serializer_class = CollectionSerializer
-
     def delete(self, request, pk):
-        collection = get_object_or_404(
-            Collection.objects.annotate(
-                products_count=Count('product')), pk=pk)
+        collection = get_object_or_404(Collection, pk=pk)
         if collection.product_set.count() > 0:
             return Response({'error': 'Collection cannot be deleted because it includes one or more products.'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
-            collection.delete()
+        collection.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
-    
