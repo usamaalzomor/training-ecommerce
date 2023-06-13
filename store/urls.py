1c32084b7d8c4c842d8e757256a838263062ff63
Usamaalzomor <osama.zomor2010@gmail.com>
2023-06-13 21:02:30 +0300

    feat(store): Add collection_id URL filter to ProductViewSet Implemented URL parameter-based filtering in ProductViewSet to allow clients to filter products by 'collection_id'.

--- store/urls.py
+++ store/urls.py
@@ -4,10 +4,10 @@ from rest_framework_nested import routers
 from .views import ProductViewSet, CollectionViewSet, ReviewViewSet
 
 router = routers.DefaultRouter()
-router.register(r'products', ProductViewSet)
+router.register(r'products', ProductViewSet, basename='products')
 router.register(r'collections', CollectionViewSet)
 
-products_router = routers.NestedDefaultRouter(router, r"products", lookup = 'product')
+products_router = routers.NestedDefaultRouter(router, r"products", lookup='product')
 products_router.register(r'reviews', ReviewViewSet, basename='product-reviews')
 
 # URLConf
