aa0ecaef7cab0a0795522d238b6e6267372264b1
Usamaalzomor <osama.zomor2010@gmail.com>
2023-06-19 22:52:34 +0300

    fix(feat): Add uuidfield to crat model

--- store/models.py
+++ store/models.py
@@ -1,5 +1,6 @@
 from django.core.validators import MinValueValidator
 from django.db import models
+from uuid import uuid4
 
 
 class Promotion(models.Model):
@@ -95,6 +96,7 @@ class Address(models.Model):
 
 
 class Cart(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4)
     created_at = models.DateTimeField(auto_now_add=True)
 
 
