64a7b102ed06c53773514bff3e75e70f995b41b7
Usamaalzomor <osama.zomor2010@gmail.com>
2023-06-20 22:34:40 +0300

    refactor(store): Change cart route to 'carts'

--- store/urls.py
+++ store/urls.py
@@ -6,7 +6,7 @@ from .views import ProductViewSet, CollectionViewSet, ReviewViewSet, CartViewSet
 router = routers.DefaultRouter()
 router.register(r'products', ProductViewSet, basename='products')
 router.register(r'collections', CollectionViewSet)
-router.register(r'cart', CartViewSet)
+router.register(r'carts', CartViewSet)
 
 products_router = routers.NestedDefaultRouter(router, r"products", lookup='product')
 products_router.register(r'reviews', ReviewViewSet, basename='product-reviews')
