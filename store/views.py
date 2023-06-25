d52e1b80519bf9452039a53b3775e50cd817cd08
Usamaalzomor <osama.zomor2010@gmail.com>
2023-06-25 14:50:42 +0300

    perf(store): Optimize CartItemViewSet queryset with selectrelated

--- store/views.py
+++ store/views.py
@@ -60,4 +60,5 @@ class CartItemViewSet(ModelViewSet):
     serializer_class = CartItemSerializer
 
     def get_queryset(self):
-        return CartItem.objects.filter(cart_id=self.kwargs['cart_pk'])
+        return CartItem.objects.filter(cart_id=self.kwargs['cart_pk'])\
+        .select_related('product')
