3454b6057e3378b637297e10a27b1a13535a55da
Usamaalzomor <osama.zomor2010@gmail.com>
2023-06-29 17:45:58 +0300

    feat(core): Customize UserSerializer of Djoser
    Summary:
    - Override Djoser's default UserSerializer with a custom serializer that extends the default one.
    - Include 'id', 'username', 'email', 'first_name', and 'last_name' fields in the custom serializer.
    - Update Djoser's settings in settings.py to use the custom serializer.

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -156,7 +156,9 @@ REST_FRAMEWORK = {
 AUTH_USER_MODEL = 'core.User'
 
 DJOSER = {
-    "SERIALIZERS": {'user_create': 'core.serializers.UserCreateSerializer',},
+    "SERIALIZERS": {'user_create': 'core.serializers.UserCreateSerializer',
+                    'current_user': 'core.serializers.UserSerializer',
+                    },
 }
 
 SIMPLE_JWT = {
