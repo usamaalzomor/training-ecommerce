195bd790fe0667e2476b2586dc89bc440e34e566
Usamaalzomor <osama.zomor2010@gmail.com>
2023-06-30 23:34:25 +0300

    feat(store): Apply permissions to store app views
    Description:
    - Created a custom permission class 'IsAdminOrReadOnly' in 'permissions.py'.
    - Applied 'IsAdminOrReadOnly' permission class to 'ProductViewSet' and 'CollectionViewSet'.
    - Applied 'IsAdminUser' permission class to 'CustomerViewSet'.
    - Applied 'IsAuthenticated' permission to the 'me' action within 'CustomerViewSet'

--- store/views.py
+++ store/views.py
@@ -5,7 +5,9 @@ from rest_framework import status, filters
 from rest_framework.viewsets import ModelViewSet, GenericViewSet
 from rest_framework.decorators import action
 from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin, DestroyModelMixin, UpdateModelMixin
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from django_filters.rest_framework import DjangoFilterBackend
+from store.permissions import IsAdminOrReadOnly
 from .models import Collection, Customer, Product, Review, Cart, CartItem
 from .serializers import CustomerSerializer, ProductSerializer, CollectionSerializer, ReviewSerializer, CartSerializer, CartItemSerializer, AddCartItemSerializer, UpdateCartItemSerializer
 from .filters import ProductFilter
@@ -18,6 +20,7 @@ class ProductViewSet(ModelViewSet):
     filter_backends = (DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter)
     filterset_class = ProductFilter
     pagination_class = DefaultPagination
+    permission_classes = [IsAdminOrReadOnly]
     search_fields = ["title"]
     ordering_fields = ['title', "last_update", "unit_price"]
 
@@ -31,6 +34,7 @@ class ProductViewSet(ModelViewSet):
 class CollectionViewSet(ModelViewSet):
     queryset = Collection.objects.annotate(products_count=Count('product')).all()
     serializer_class = CollectionSerializer
+    permission_classes = [IsAdminOrReadOnly]
 
     def destroy(self, request, *args, **kwargs):
         collection = self.get_object()
@@ -79,11 +83,12 @@ class CartItemViewSet(ModelViewSet):
         .select_related('product')
     
 
-class CustomerViewSet(CreateModelMixin, RetrieveModelMixin, UpdateModelMixin, GenericViewSet):
+class CustomerViewSet(ModelViewSet):
     queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
+    permission_classes =  [IsAdminUser]
 
-    @action(detail=False, methods=['GET', 'PUT'])
+    @action(detail=False, methods=['GET', 'PUT'], permission_classes=[IsAuthenticated])
     def me(self, request):
         customer, created = Customer.objects.get_or_create(user_id=request.user.id)
         if request.method == 'GET':
