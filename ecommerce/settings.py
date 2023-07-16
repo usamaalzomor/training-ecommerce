f17ee8174d1228953b2898ca91dd2d67d3a7b2b6
Usamaalzomor <osama.zomor2010@gmail.com>
2023-07-16 20:24:41 +0300

    feat: Implement django-cors-headers for CORS support
    Description:
    - django-cors-headers package added to Django middleware
    - CORS_ALLOWED_ORIGINS defined in settings.py
    - Allowed origins include localhost:8001 and 127.0.0.1:8001

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'playground',
     "debug_toolbar",
     "django_filters",
+    "corsheaders",
     'likes',
     'store',
     'tags',
@@ -50,6 +51,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -64,6 +66,11 @@ INTERNAL_IPS = [
     # ...
     "127.0.0.1",
     # ...
+]
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:8001',
+    'http://127.0.0.1:8001'
 ]
 
 ROOT_URLCONF = 'ecommerce.urls'
