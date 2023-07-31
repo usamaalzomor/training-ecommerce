ba7f4673d0bb2d02d06846e4c068671a6ca765a4
Usamaalzomor <osama.zomor2010@gmail.com>
2023-07-31 18:47:52 +0300

    test(store): Add test cases for CartViewSet
    - Implement setup method for creating test Product and Collection.
    - Add test for creating a cart (POST /store/carts/).
    - Add test for retrieving a cart (GET /store/carts/{cart.id}/).
    - Add test for destroying a cart (DELETE /store/carts/{cart.id}/).

--- store/tests.py
+++ store/tests.py
@@ -1,3 +1,32 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework import status
+from django.urls import reverse
+from .models import Cart, Product, Collection
 
-# Create your tests here.
+class CartViewSetTestCase(APITestCase):
+    def setUp(self):
+        # Assuming you have a Product model related to your Cart.
+        collection = Collection.objects.create(title='Test Collection')
+        self.product = Product.objects.create(title='Test Product', unit_price=100.0, inventory = 1, collection=collection)
+
+
+    def test_create_cart(self):
+        response = self.client.post('/store/carts/', {})
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Cart.objects.count(), 1)
+
+
+    def test_retrieve_cart(self):
+        # Create a Cart object for testing retrieve operation.
+        cart = Cart.objects.create()
+        response = self.client.get(f'/store/carts/{cart.id}/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+    def test_destroy_cart(self):
+        # Create a Cart object for testing destroy operation.
+        cart = Cart.objects.create()
+        self.assertEqual(Cart.objects.count(), 1)
+        response = self.client.delete(f'/store/carts/{cart.id}/')
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(Cart.objects.count(), 0)
